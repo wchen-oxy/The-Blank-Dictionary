c9d6a7e637eb59fccdc2440471a4248b1a3ee811
William S Chen <blackjamx5@hotmail.com>
2019-11-07 14:25:55 -0800

    First Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y)i1bq+nsu0377az=6+4eq5utii#bn$$9ywc)t!q1ypa@d%e@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.heroku.com', '.herokuapp.com']
 
 
 # Application definition
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -12,25 +12,19 @@ import dictionaries
 from django.views.decorators.csrf import csrf_exempt
 import hashlib
 
-
 #API TEST STUFF
-
-
 def home(request):
     # return HttpResponse("Hello, world. You're at the home index.")
     home = True
     return render(request, 'home.html', {'home': home})
 
-
 def test(request):
     return HttpResponse("Site works!")
-
 
 def download(request, pack):
     data = serializers.serialize(
         "json", apps.get_model("mainapp", pack).objects.all())
     return JsonResponse(data, safe=False)
-
 
 class AllBhutia(generics.ListAPIView):
     """
@@ -43,14 +37,12 @@ class AllBhutia(generics.ListAPIView):
     queryset = target.objects.all()
     serializer_class = BhutiaSerializer
 
-
 @csrf_exempt
 def test(request, lang, word=None):
     #  if request.method == 'GET':
      if request.method == 'GET' and 'Authorization' in request.headers:
          if request.headers['Authorization'] != encrypt_string("Az39dB0n!23"):
              return HttpResponse("Auth Key is Wrong.")
-        #  print(encrypt_string(request.headers['Authorization']))
         ##FIXME NEED TO DESIGN INDIVIDUAL WORD SEARCH
          if (word != None):
              dictionary = apps.get_model("dictionaries", lang)
@@ -59,8 +51,6 @@ def test(request, lang, word=None):
              serializer = BhutiaSerializer(dic, many=True)
              return JsonResponse(serializer.data, safe=False)
 
-        #  dictionary = getattr(d, lang)
-        #  dictionary = d.Bhutia
          dictionary = apps.get_model("dictionaries", lang)
          print(dictionary)
          dic = dictionary.objects.all()
