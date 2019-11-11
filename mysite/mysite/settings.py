da5d6f2dfd82b5f6da8eef3dbd410fa02db399c7
William S Chen <blackjamx5@hotmail.com>
2019-11-11 11:18:39 -0800

    f

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
 
-import urllib3.parse as urlparse
+import urllib.parse as urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
